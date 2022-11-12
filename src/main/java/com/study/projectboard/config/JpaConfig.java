4b92212424b4de92378e3ada09f6779168a97dbb
heechanshin <hs1916@gmail.com>
2022-11-12 18:37:35 +0900

    #10 - 의존성 업데이트, 속성 설정: JPA + MYSQL + H2
    Springboot 에서 DB 와 JPA 사용하게끔 의존성 추가
    JPA 속성 설정
    * DB 접근과 개발 편의를 위한 각종 부트 프로퍼티 적용
    * TODO : DB 계정정보 암호화

---
+++ src/main/java/com/study/projectboard/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.study.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("heechan"); // TODO 스프링 인증 시큐리티로 수정 필요
+    }
+
+}
