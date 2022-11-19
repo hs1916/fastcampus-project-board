a162431bc4c489e1e6e78e82187847011bec4406
heechanshin <hs1916@gmail.com>
2022-11-19 19:31:03 +0900

    #21 - 전체 커밋

--- src/main/java/com/study/projectboard/domain/Article.java
+++ src/main/java/com/study/projectboard/domain/Article.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(indexes = {
         @Index(columnList = "title"),
@@ -18,7 +18,6 @@ import java.util.Set;
 
 })
 @Entity
-
 public class Article extends AuditingFields {
 
     @Id
@@ -26,8 +25,13 @@ public class Article extends AuditingFields {
     private Long id;
 
     @Setter
+    @ManyToOne(optional = false)
+    private UserAccount userAccount;
+
+    @Setter
     @Column(nullable = false)
     private String title; // 제목
+
     @Setter
     @Column(nullable = false, length = 10000)
     private String content; // 내용
@@ -36,18 +40,19 @@ public class Article extends AuditingFields {
     private String hashtag; // 해시태그
 
     @ToString.Exclude
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     @Override
---
+++ src/main/java/com/study/projectboard/dto/ArticleUpdateDto.java
@@ -0,0 +1,17 @@
+package com.study.projectboard.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * A DTO for the {@link com.study.projectboard.domain.Article} entity
+ */
+public record ArticleUpdateDto(
+        String title,
+        String content,
+        String hashtag
+) {
+    public static ArticleUpdateDto of( String title, String content, String hashtag) {
+        return new ArticleUpdateDto(title, content, hashtag);
+    }
+}
