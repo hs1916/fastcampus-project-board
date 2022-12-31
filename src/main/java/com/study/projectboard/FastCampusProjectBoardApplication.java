cd5c3317a67d9639062885559b2a7cbca34e188c
heechanshin <hs1916@gmail.com>
2022-12-31 19:44:50 +0900

    #47-heroku 자동 배포 기능 이용

--- src/main/java/com/study/projectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/study/projectboard/FastCampusProjectBoardApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 public class FastCampusProjectBoardApplication {
 
-	// 실제 주석 추가 변경점 잡기 위함
-
 	public static void main(String[] args) {
 		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
 	}
