2dfc9401504e01a3eee032d28e74a513292ea822
heechanshin <hs1916@gmail.com>
2022-12-31 18:43:36 +0900

    #43 - test code 수정
    수정

--- src/test/java/com/study/projectboard/controller/MainControllerTest.java
+++ src/test/java/com/study/projectboard/controller/MainControllerTest.java
@@ -1,20 +1,29 @@
 package com.study.projectboard.controller;
 
 import com.study.projectboard.config.SecurityConfig;
+import com.study.projectboard.config.TestSecurityConfig;
+import com.study.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest
 class MainControllerTest {
 
     private final MockMvc mockMvc;
+
+    @MockBean
+    ArticleCommentController articleCommentController;
+
+    @MockBean
+    ArticleService articleService;
 
     @Autowired
     public MainControllerTest(MockMvc mockMvc) {
